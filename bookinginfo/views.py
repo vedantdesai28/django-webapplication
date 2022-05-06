42f4b1f8b77ca05a548cc4914dd3baca6adff135
Vedant Desai <vedant.desai@graybar.com>
2022-05-06 10:08:52 -0500

    Vedant-desai:Added pagination to the pages

--- bookinginfo/views.py
+++ bookinginfo/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
@@ -13,14 +14,59 @@ from bookinginfo.models import (
 from bookinginfo.utils import ObjectCreateMixin
 
 
+# class LocationList(View):
+#
+#     def get(self, request):
+#         return render(
+#             request,
+#             'bookinginfo/location_list.html',
+#             {'location_list': LocationDetail.objects.all()}
+#         )
+
+
 class LocationList(View):
+    page_kwarg = 'page'
+    paginate_by = 10;
+    template_name = 'bookinginfo/location_list.html'
 
     def get(self, request):
-        return render(
-            request,
-            'bookinginfo/location_list.html',
-            {'location_list': LocationDetail.objects.all()}
+        locations = LocationDetail.objects.all()
+        paginator = Paginator(
+            locations,
+            self.paginate_by
         )
+        page_number = request.GET.get(
+            self.page_kwarg
+        )
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(
+                paginator.num_pages)
+        if page.has_previous():
+            prev_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.previous_page_number())
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.next_page_number())
+        else:
+            next_url = None
+        context = {
+            'is_paginated':
+                page.has_other_pages(),
+            'next_page_url': next_url,
+            'paginator': paginator,
+            'previous_page_url': prev_url,
+            'location_list': page,
+        }
+        return render(
+            request, self.template_name, context)
 
 
 class LocationDetails(View):
@@ -434,14 +480,58 @@ class SemesterDelete(View):
         return redirect('bookinginfo_semester_list_urlpattern')
 
 
+# class UserList(View):
+#
+#     def get(self, request):
+#         return render(
+#             request,
+#             'bookinginfo/user_list.html',
+#             {'user_list': User.objects.all()}
+#         )
+
 class UserList(View):
+    page_kwarg = 'page'
+    paginate_by = 10;  # 25 instructors per page
+    template_name = 'bookinginfo/user_list.html'
 
     def get(self, request):
-        return render(
-            request,
-            'bookinginfo/user_list.html',
-            {'user_list': User.objects.all()}
+        users = User.objects.all()
+        paginator = Paginator(
+            users,
+            self.paginate_by
         )
+        page_number = request.GET.get(
+            self.page_kwarg
+        )
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(
+                paginator.num_pages)
+        if page.has_previous():
+            prev_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.previous_page_number())
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.next_page_number())
+        else:
+            next_url = None
+        context = {
+            'is_paginated':
+                page.has_other_pages(),
+            'next_page_url': next_url,
+            'paginator': paginator,
+            'previous_page_url': prev_url,
+            'user_list': page,
+        }
+        return render(
+            request, self.template_name, context)
 
 
 class UserDetail(View):
